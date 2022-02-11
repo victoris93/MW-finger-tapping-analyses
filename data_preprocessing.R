c7e65cfd0800a04e455812c0107f91c2621c8376
Victoria Shevchenko <victoria.shevchenko@ens.psl.eu>
2022-02-11 14:41:23 +0100

    nback function fixed

--- data_preprocessing.R
+++ data_preprocessing.R
@@ -1,6 +1,15 @@
 
-library(reshape2)
-data.path="/Users/VictoriaShevchenko/Documents/STAGE_M2/Experiment/Analyses/data"
+library(knitr)
+library(dplyr)
+library(purrr)
+library(Rcpp)
+library(dplyr)
+library(ggplot2)
+library(readr)
+library(forcats)
+sourceCpp("apen.cpp")
+
+data.path="/Users/VictoriaShevchenko/Documents/STAGE_M2/Analyses/data"
 
 data_files_baseline=list.files(path=data.path, pattern=".*_baseline_.*\\.csv", full.names = T )
 data_files_rhTMS=list.files(path=data.path, pattern=".*_rhTMS_.*\\.csv", full.names = T )
@@ -33,41 +42,41 @@ all_data <- all_data %>% mutate(distraction = case_when(stimulus == "probe_task"
 
 all_data$intention[all_data$intention == 0] <- "intentional"
 all_data$intention[all_data$intention == 1] <- "spontaneous"
-
+all_data[20877, "intention"] <- "intentional"
+all_data[20877, "distraction"] <- 0
 
 intention_distraction_values <- all_data[,c("intention", "distraction")] %>% 
   filter(!is.na(intention) & !is.na(distraction))
 
 all_data %>% filter(stimulus == "probe_task")
 
-all_data_nback <- cbind(all_data_nback, intention_distraction_values)
 #all_data$response[all_data$response == 3] <- 4
 #all_data$response[all_data$response == 2] <- 3
 #all_data$response[all_data$response == 1] <- 2
 #all_data$response[all_data$response == 0] <- 1
 
-get.nback_v <- function(d, nback=20, which.apen.m=3, on.task.crit=1){ #, on.task.crit=1
- # if( !("part" %in% names(d))){
- #   d$part=1 ## this is for pilot2
- # }
-  
-  d %>% filter(stimulus=="probe_task") %>%  #  |stimulus=="probe_intention" | stimulus=="probe_distraction") %>%
-    mutate(attention=if_else(as.integer(response)<=on.task.crit, "on-task", "off-task")) %>%
+get.nback <- function(d, nback=20, which.apen.m=3, on.task.crit=2){
+  if( !("condition" %in% names(d))){
+    d$condition=1 ## this is for pilot2
+  }
+  d %>% filter(stimulus=="probe_task") %>% 
+    mutate(attention=if_else(as.integer(response)>=on.task.crit, "on-task", "off-task")) %>%
     group_by(subj,condition) %>% 
     do({
       df=.
       df %>% mutate(probeix=1:n()) -> df
       dd=d %>% filter(subj==df$subj[1], condition==df$condition[1])
-      probeix=which(dd$stimulus=="probe_task") #|dd$stimulus=="probe_intention" | dd$stimulus=="probe_distraction")
+      probeix=which(dd$stimulus=="probe_task")
       nprobes=length(probeix)
       dd$probeix=rep(c(1:nprobes, -1), diff(c(0,probeix,dim(dd)[1])))
       
       trials=map(1:nback, function(x) df$trial-x) %>% unlist
       dd %>%
         filter(trial %in% trials) %>%
+        filter(stimulus != "probe_task" & stimulus != "probe_intention" & stimulus != "probe_distraction") %>%
         mutate(focus=map_chr(probeix, function(t){
           df$attention[t]}),
-          probe.response=map_int(probeix, function(t){ !
+          probe.response=map_int(probeix, function(t){
             as.integer(df$response[t])
           })+1)
     }) %>% ungroup -> d.nback
@@ -81,14 +90,14 @@ get.nback_v <- function(d, nback=20, which.apen.m=3, on.task.crit=1){ #, on.task
   
   
   d.nback %>% 
-    group_by(condition, subj, probeix, focus,probe.response) %>% #focus,
+    group_by(condition, subj, focus, probeix,probe.response) %>%
     summarize(apen=apen_int(tap,3)[which.apen.m+1]) %>%
-    mutate(log.apen=log(log(2)-apen))  %>% ungroup -> d.nback.apen
+    mutate(log.apen=log(log(2)-apen)) %>% ungroup -> d.nback.apen
   
   
-  d.nback %>% group_by(subj,focus,condition,probeix) %>% #focus,
+  d.nback %>% group_by(subj,condition,focus,probeix) %>%
     mutate(timediff=c(0,diff(time))) %>% filter(timediff>0) %>%
-    summarise(bv=sd(timediff)) %>%  ungroup -> d.nback.bv
+    summarise(bv=sd(timediff)) %>% ungroup -> d.nback.bv
   
   d.nback.apen %>% 
     full_join(d.nback.bv) %>% ungroup -> d.nback.full
@@ -96,15 +105,14 @@ get.nback_v <- function(d, nback=20, which.apen.m=3, on.task.crit=1){ #, on.task
   d.nback.full %>% ungroup %>% mutate(zlog.apen=-(log.apen-mean(log.apen, na.rm=T))/sd(log.apen,na.rm=T),
                                       zbv=(bv-mean(bv,na.rm=T))/sd(bv,na.rm=T)) %>%
     mutate(focus=factor(focus)) %>%
-    mutate(off.focus=as.integer(focus=="off-task")) %>%
-    ungroup -> d.nback.full.z
+    mutate(off.focus=as.integer(focus=="off-task")) -> d.nback.full.z
   
   
   
   return(d.nback.full.z)
 }
 
-all_data_nback <- get.nback_v(all_data, nback = 20, which.apen.m=2)
+all_data_nback <- get.nback(all_data, nback = 20, which.apen.m=2)
 
 d %>% 
   filter(stimulus == "probe1") %>%
